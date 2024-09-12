7a69d8c99c32eb8c2531d956b80dc50ad829a93e
KonstantinChuper <konstantyn.chuper@gmail.com>
2024-09-12 15:47:36 +0200

    hw-8

--- hw-8/app.js
+++ hw-8/app.js
@@ -1,12 +1,67 @@
 import express from "express";
 import sequelize from "./config/db.js";
+import Book from "./models/book.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send("Book shop");
+});
+
+app.get("/books", async (req, res) => {
+  try {
+    const books = await Book.findAll();
+    res.json(books);
+  } catch (error) {
+    res.status(500).send("Error to get books");
+  }
+});
+
+app.post("/books", async (req, res) => {
+  try {
+    const { title, author, year } = req.body;
+    if (!title || !author || !year) {
+      return res.status(400).json({ error: "Fill out all fields" });
+    }
+    const addedBook = await Book.create({ title, author, year });
+    res.status(201).json(addedBook);
+  } catch (error) {
+    console.error("Error to create book:", error);
+    res.status(500).send("Error to create book");
+  }
+});
+
+app.put("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, author, year } = req.body;
+    const book = await Book.findByPk(id);
+    if (!book) {
+      res.status(404).send("Book not found");
+    }
+    await book.update({ title, author, year });
+    res.status(200).json(book);
+  } catch (error) {
+    console.error(error);
+    app.status(500).send("Error to change book");
+  }
+});
+
+app.delete("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await Book.findByPk(id);
+    if (!book) {
+      res.status(404).send("Book not found");
+    }
+    await book.destroy();
+    res.status(200).send("Book deleted successfully");
+  } catch (error) {
+    console.error(error);
+    app.status(500).send("Error to delete book");
+  }
 });
 
 app.listen(port, async () => {
@@ -16,6 +71,5 @@ app.listen(port, async () => {
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
-
   console.log(`Server is up and running on http://localhost:${port}`);
 });
--- hw-8/models/book.js
+++ hw-8/models/book.js
@@ -1,25 +1,31 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/db.js";
 
-const Books = sequelize.define("Books", {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-    allowNull: false,
+const Book = sequelize.define(
+  "Books",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    year: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
   },
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  author: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  year: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-});
+  {
+    timestamps: false,
+  }
+);
 
-export default Books;
+export default Book;
